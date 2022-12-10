76d0d59c4227bc995f104eafea700c51f4e94d31
Said Saliev <repsaid@gmail.com>
2022-12-10 11:39:53 +0600

    crate models Pool, Choice, migrate, add models in admin panel

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from webapp.models import Poll, Choice
 # Register your models here.
+
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['question']
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['variant']
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice, ChoiceAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Poll(models.Model):
+    question = models.TextField(max_length=30, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата начала")
+
+
+class Choice(models.Model):
+    variant = models.TextField(max_length=30, verbose_name='Текст варианта')
+    poll = models.ForeignKey('webapp.Poll', on_delete=models.CASCADE, related_name='pools')
