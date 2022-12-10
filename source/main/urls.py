1ca755eddfea030b959106f47a906d81f5ca4ca7
Said Saliev <repsaid@gmail.com>
2022-12-10 12:13:55 +0600

    add detail view for poll

--- source/main/urls.py
+++ source/main/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from webapp.views import PollIndex
+from webapp.views import PollIndex, PollView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', PollIndex.as_view(), name='poll_index')
+    path('', PollIndex.as_view(), name='poll_index'),
+    path('pool/<int:pk>/', PollView.as_view(), name='poll')
 ]
--- source/webapp/views/poll_views.py
+++ source/webapp/views/poll_views.py
@@ -1,9 +1,14 @@
 from webapp.models import Poll
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 # Create your views here.
 
 
 class PollIndex(ListView):
     template_name = 'poll/poll_index.html'
     context_object_name = 'polls'
+    model = Poll
+
+
+class PollView(DetailView):
+    template_name = 'poll/poll_view.html'
     model = Poll
